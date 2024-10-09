80da1581a3c1c8c3e274ce010ff47a25d67ed073
Arun kk <arunkk1872002@gmail.com>
2024-10-09 12:35:20 +0530

    Initial commit

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: AppPallete.borderColor
+        )
+      )
+    )
+  );
+}
